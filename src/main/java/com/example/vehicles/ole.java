6fb7bc129692ca0ca96a68b06c761b6472c07e0f
Pramod Tanaji Phadtare <pramod.phadtare@newgensoft.com>
2024-02-05 17:05:12 +0530

    adding new ole

---
+++ src/main/java/com/example/vehicles/ole.java
@@ -0,0 +1,11 @@
+package com.example.vehicles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ole {
+
+    @GetMapping ("/ole")
+    public String getData() {return  "Please book ole at 10% Discount" ; }
+}
