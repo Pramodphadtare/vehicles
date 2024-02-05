3cc0b88866905a82778eabcfa2ed7acff52dbc4c
Pramod Tanaji Phadtare <pramod.phadtare@newgensoft.com>
2024-02-05 16:30:46 +0530

    adding ola

---
+++ src/main/java/com/example/vehicles/ola.java
@@ -0,0 +1,11 @@
+package com.example.vehicles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ola {
+
+    @GetMapping ("/ola")
+    public String getData() {return  "Please book ola at 12% Discount" ; }
+}
