3a35a124fd4a2c2778a1bfa3cf78a26e27df8a3d
pramod <pramodphadtare5@gmail.com>
2024-02-06 11:27:47 +0530

    porshe added

---
+++ src/main/java/com/example/vehicles/porshe.java
@@ -0,0 +1,11 @@
+package com.example.vehicles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class porshe {
+
+    @GetMapping ("/porshe")
+    public String getData() {return  "Please book porshe at 20% Discount" ; }
+}
